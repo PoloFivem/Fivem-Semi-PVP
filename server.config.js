56a00446cd18c122f27c7c7707db02cb8891fc49
PoloFivem <106012206+PoloFivem@users.noreply.github.com>
2022-05-22 11:35:37 +0200

    Delete server.config.js

--- server.config.js
+++
@@ -1,7 +0,0 @@
-const Config = {}
-
-Config.GuildId = "950840120792195073"
-Config.AuthKey = "OTU5NjA5NDc5OTMzMDk1OTM2.YkeX-A.igPbXCWMPBElTkWT9YWLzpZzFZI"
-
-Config.WhitelistId = "950862740619149352"
-Config.CriminalId = "950863153439309984"
